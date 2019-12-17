b85f0276c2dabdc01947083f567d95b520d552d0
Mario <carmar141414@hotmail.com>
2019-12-17 10:02:59 -0500

    Generacion de carga y renovables
    Generacion de archivos para carga y renovables unidos

---
+++ Nuevos signal flow simulation/carga_reno_ert_rtw/carga_reno_data.c
@@ -0,0 +1,36 @@
+/*
+ * File: carga_reno_data.c
+ *
+ * Code generated for Simulink model 'carga_reno'.
+ *
+ * Model version                  : 1.21
+ * Simulink Coder version         : 8.14 (R2018a) 06-Feb-2018
+ * C/C++ source code generated on : Fri Dec 13 10:39:26 2019
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "carga_reno.h"
+#include "carga_reno_private.h"
+
+/* Constant parameters (default storage) */
+const ConstP_carga_reno_T carga_reno_ConstP = {
+  /* Expression: [0 10 200 500 800 1000 1140 1150 1200]
+   * Referenced by: '<Root>/1-D Lookup Table2'
+   */
+  { 0.0, 10.0, 200.0, 500.0, 800.0, 1000.0, 1140.0, 1150.0, 1200.0 },
+
+  /* Expression: [0 10 20 30 40 50 70 80 100]
+   * Referenced by: '<Root>/1-D Lookup Table2'
+   */
+  { 0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 70.0, 80.0, 100.0 }
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
