15642dd3406de2f06f34ce05a87a68a5678ec1d2
carmar14 <carmar141414@hotmail.com>
2021-11-26 14:33:57 -0500

    Resultados
    Graficas para tesis, todo en el mismo formato

---
+++ Nuevos signal flow simulation/base_graficas.m
@@ -0,0 +1,4 @@
+ylabel('$V$','fontsize',14,'interpreter','latex')
+xlabel('$Tiempo(seg)$','fontsize',14,'interpreter','latex')
+legend('$V_{carga}$','$Ref$','fontsize',14,'interpreter','latex')
+title('Voltaje en la carga','fontsize',14,'interpreter','latex')
